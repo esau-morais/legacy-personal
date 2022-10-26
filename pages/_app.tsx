92d4cd31947f8bc611976c5275b90879a6efe359
Esaú Morais <esaumorais7@gmail.com>
2022-10-26 02:26:43 +0000

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { DefaultSeo } from 'next-seo'
+import { Analytics } from '@vercel/analytics/react';
 import { seo } from '@/lib/seo'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <DefaultSeo {...seo} />
+      <Analytics />
       <Component {...pageProps} />
+      {/* TODO: change this soon to OG Image Edge Generation */}
+      <DefaultSeo {...seo} />
     </>
   )
 }
