cc11089ca290c4b3cf8a37af26f8379b5f489d3e
esau-morais <esaumorais7@gmail.com>
2022-11-05 16:48:48 -0300

    refactor: upgrade to next@13

--- components/organisms/Navigation.tsx
+++ components/organisms/Navigation.tsx
@@ -10,39 +10,41 @@ import TwitterIcon from '../atoms/TwitterIcon'
 const Navigation = () => {
   return (
     <div className="flex items-center leading-none space-x-7 text-rose-100/90 sm:text-lg">
-      <Link href="/projects">
-        <a className={cx('group rounded-lg', FOCUS_VISIBLE_OUTLINE)}>
-          <div className="sm:flex sm:items-center sm:space-x-2">
-            <div className="mb-1.5 flex justify-center sm:mb-0 sm:block">
-              <div className="rounded-lg bg-gradient-to-tl from-blue-400/80 to-purple-600/80 p-1 shadow-lg transition-all duration-300 ease-out hover:scale-[1.2] hover:rounded-[10px] active:scale-100 active:rounded-lg group-hover:shadow-purple-500/50">
-                <Squares2X2Icon
-                  aria-label="projects"
-                  className="w-[18px] transform text-rose-100 transition delay-100 duration-500 ease-out hover:scale-110"
-                />
-              </div>
-            </div>
-            <div className="transition-colors hover:text-rose-200/90">
-              Projects
+      <Link
+        href="/projects"
+        className={cx('group rounded-lg', FOCUS_VISIBLE_OUTLINE)}>
+
+        <div className="sm:flex sm:items-center sm:space-x-2">
+          <div className="mb-1.5 flex justify-center sm:mb-0 sm:block">
+            <div className="rounded-lg bg-gradient-to-tl from-blue-400/80 to-purple-600/80 p-1 shadow-lg transition-all duration-300 ease-out hover:scale-[1.2] hover:rounded-[10px] active:scale-100 active:rounded-lg group-hover:shadow-purple-500/50">
+              <Squares2X2Icon
+                aria-label="projects"
+                className="w-[18px] transform text-rose-100 transition delay-100 duration-500 ease-out hover:scale-110"
+              />
             </div>
           </div>
-        </a>
+          <div className="transition-colors hover:text-rose-200/90">
+            Projects
+          </div>
+        </div>
+
       </Link>
-      <Link href="/blog">
-        <a className={cx('group rounded-lg', FOCUS_VISIBLE_OUTLINE)}>
-          <div className="sm:flex sm:items-center sm:space-x-2">
-            <div className="mb-1.5 flex justify-center sm:mb-0 sm:block">
-              <div className="rounded-lg bg-gradient-to-tl from-blue-400/80 to-purple-600/80 p-1 shadow-lg transition-all duration-300 ease-out hover:scale-[1.2] hover:rounded-[10px] active:scale-100 active:rounded-lg group-hover:shadow-purple-500/50">
-                <ChatBubbleBottomCenterTextIcon
-                  aria-label="Posts"
-                  className="w-[18px] transform text-rose-100 transition delay-100 duration-500 ease-out hover:scale-110"
-                />
-              </div>
-            </div>
-            <div className="transition-colors hover:text-rose-200/90">
-              Posts
+      <Link href="/blog" className={cx('group rounded-lg', FOCUS_VISIBLE_OUTLINE)}>
+
+        <div className="sm:flex sm:items-center sm:space-x-2">
+          <div className="mb-1.5 flex justify-center sm:mb-0 sm:block">
+            <div className="rounded-lg bg-gradient-to-tl from-blue-400/80 to-purple-600/80 p-1 shadow-lg transition-all duration-300 ease-out hover:scale-[1.2] hover:rounded-[10px] active:scale-100 active:rounded-lg group-hover:shadow-purple-500/50">
+              <ChatBubbleBottomCenterTextIcon
+                aria-label="Posts"
+                className="w-[18px] transform text-rose-100 transition delay-100 duration-500 ease-out hover:scale-110"
+              />
             </div>
           </div>
-        </a>
+          <div className="transition-colors hover:text-rose-200/90">
+            Posts
+          </div>
+        </div>
+
       </Link>
 
       <a
@@ -64,7 +66,7 @@ const Navigation = () => {
         </div>
       </a>
     </div>
-  )
+  );
 }
 
 export default Navigation
--- pages/404.tsx
+++ pages/404.tsx
@@ -11,13 +11,15 @@ const NotFound = () => {
       <p className="my-4 text-lg text-rose-50/80">
         This page doesn&lsquo;t exist or was removed &#128533;
       </p>
-      <Link href="/">
-        <a className="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold uppercase tracking-widest transition ease-in-out duration-150 bg-gradient-to-tl from-blue-400/80 to-purple-600/80 text-white text-md mt-4">
+      <Link
+        href="/"
+        className="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold uppercase tracking-widest transition ease-in-out duration-150 bg-gradient-to-tl from-blue-400/80 to-purple-600/80 text-white text-md mt-4">
+        
           Go back home
-        </a>
+        
       </Link>
     </Layout>
-  )
+  );
 }
 
 export default NotFound
